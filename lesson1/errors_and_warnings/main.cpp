f28189e27fcd3076cdb0167961a40f03621c50cd
Rotem Tsalisher <Rotemt@cardosystems.com>
2025-11-13 11:40:05 +0200

    first lesson, laptop, 13_11

---
+++ lesson1/errors_and_warnings/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define SUCCESS 0
+
+//#define COMP_ERR_
+//#define RUN_ERR_
+//#define WARNING_
+
+int main()
+{
+#ifdef COMP_ERR_
+	std::cout << "Hello Wotld in C++20!" << std::endl
+#endif
+#ifdef RUN_ERR_
+	int i = 0;
+	for (i;;i++);
+#endif
+#ifdef WARNING_
+	int x = 20;
+	float y = x;
+#endif
+	std::cout << "Goodbye!" << std::endl;
+	return SUCCESS;
+}
---
+++ lesson1/hello_world/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#define SUCCESS 0
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return SUCCESS;
+}
